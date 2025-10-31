6d6fdece623e33994d6100d2afebae43b07f5e7b
Smit <smitkalavadiya99@gmail.com>
2025-10-30 21:02:17 -0400

    login and signup added

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
         path('about/', TemplateView.as_view(template_name="About.html")),
+            path('register/', TemplateView.as_view(template_name="index.html")),
+        path('login/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
         path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
